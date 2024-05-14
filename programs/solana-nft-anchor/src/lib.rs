98ad33f41140299ffa4c70f525f2c05937c530c9
taiwo2 <adetonat@ymail.com>
2024-05-14 09:51:59 +0100

    Update the keys

--- programs/solana-nft-anchor/src/lib.rs
+++ programs/solana-nft-anchor/src/lib.rs
@@ -13,7 +13,7 @@ use anchor_spl::{
 };
 use mpl_token_metadata::accounts::{ MasterEdition, Metadata as MetadataAccount };
 
-declare_id!("<UPDATE HERE>");
+declare_id!("GNpD9UgjSe3pyYWu9wFrELpm65oWv8TdmExj5Waz4inV");
 #[program]
 pub mod solana_nft_anchor {
     use super::*;
